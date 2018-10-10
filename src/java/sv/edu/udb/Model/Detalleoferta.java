e3130d8e527a6992998ee593597db09e9b8448cc
Edal Bonilla <Edal Bonilla@EdalBonilla-PC>
2018-10-10 07:52:58 -0600

    Actualizacion edwin

--- src/java/sv/edu/udb/Model/Detalleoferta.java
+++ src/java/sv/edu/udb/Model/Detalleoferta.java
@@ -8,6 +8,8 @@ import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -40,6 +42,7 @@ public class Detalleoferta implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "idDetalle")
     private Integer idDetalle;
